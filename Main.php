eab3eb6fe5af926a51beb2464f03b5816d0697b5
Nathan <nathw014@gmail.com>
2022-07-25 10:48:52 +0100

    Removed readme for now. Added more to form.

--- Main.php
+++ Main.php
@@ -1,6 +1,14 @@
 <html>
+<head>
+<title>Registration Form</title>
+<style>
+.error {color: #FF0000;}
+</style>
+</head>
 <body>
+<h1>Abstract Registration Form Ltd.</h1>
 
+<h2>Please input your details below:</h2>
 <form action="Result.php" method="get">
 Name: <input type="text" name="name"><br>
 E-mail: <input type="text" name="email"><br>
--- Result.php
+++ Result.php
@@ -1,5 +1,9 @@
 <html>
+<head>
+<title>Thank you for registering!</title>
+</head>
 <body>
+<h1>Abstract Registration Form Ltd.</h1>
 
 Welcome <?php echo $_GET["name"]; ?><br>
 Your email address is: <?php echo $_GET["email"]; ?>
