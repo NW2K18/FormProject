d9d3bbc27df28f89aae6992acdd5e75ea39abe6a
Nathan <nathw014@gmail.com>
2022-07-25 11:15:20 +0100

    Added more input for the form.

--- Main.php
+++ Main.php
@@ -9,10 +9,22 @@
 <h1>Abstract Registration Form Ltd.</h1>
 
 <h2>Please input your details below:</h2>
-<form action="Result.php" method="get">
-Name: <input type="text" name="name"><br>
-E-mail: <input type="text" name="email"><br>
+<form action="Result.php" method="post">
+Name: <input type="text" name="name"><br><br>
+<!-- Use a calender input type -->
+Date of Birth: <input type="text" name="date"><br><br>
+E-mail: <input type="text" name="email"><br><br>
+Website: <input type="text" name="website"><br><br>
+Gender:
+<input type="radio" name="gender" value="male">Male
+<input type="radio" name="gender" value="female">Female
+<input type="radio" name="gender" value="other">Other<br><br>
+Comment: <textarea name="comment" rows="5" cols="40"></textarea><br><br>
+Cats or Dogs?
+<input type="radio" name="pet" value="cats">Cats
+<input type="radio" name="pet" value="dogs">Dogs<br><br>
 <input type="submit">
+
 </form>
 
 </body>
--- Result.php
+++ Result.php
@@ -5,8 +5,13 @@
 <body>
 <h1>Abstract Registration Form Ltd.</h1>
 
-Welcome <?php echo $_GET["name"]; ?><br>
-Your email address is: <?php echo $_GET["email"]; ?>
+Welcome <?php echo $_POST["name"]; ?><br>
+Your email address is: <?php echo $_POST["email"]; ?><br>
+Your date of birth is: <?php echo $_POST["date"]; ?><br>
+Your website is: <?php echo $_POST["website"]; ?><br>
+Your gender is: <?php echo $_POST["gender"]; ?><br>
+Additional comments: <?php echo $_POST["comment"]; ?><br>
+<?php echo $_POST["pet"]; ?>!
 
 </body>
 </html>
