b50b3c87ec8d957d2f32154ad2962a9a65350790
Nathan <nathw014@gmail.com>
2022-07-25 13:44:36 +0100

    Added date and time selection to form.

--- Main.php
+++ Main.php
@@ -14,9 +14,8 @@
 <label for="name">Name: </label>
 <input type="text" id="name" name="name"><br><br>
 
-<!-- Use a calender input type -->
-Date of Birth:
-<input type="text" name="date"><br><br>
+<label for="date">Date of Birth:</label>
+<input type="date" id="date" name="date" min="1900-01-01" max="2021-12-31"><br><br>
 
 <label for="email">E-mail: </label>
 <input type="text" id="email" name="email"><br><br>
@@ -24,20 +23,21 @@ Date of Birth:
 <label for="website">Website: </label>
 <input type="text" id="website" name="website"><br><br>
 
-<!-- Have a text box for Other-->
+<!-- TODO: Have a text box for Other-->
 Gender:
-<input type="radio" id="male" name="gender" value="male"><label for="male">Male</label>
-<input type="radio" id="female" name="gender" value="female"><label for="female">Female</label>
-<input type="radio" id="other" name="gender" value="other"><label for="other">Other</label><br><br>
+  <input type="radio" id="male" name="gender" value="male"><label for="male">Male</label>
+  <input type="radio" id="female" name="gender" value="female"><label for="female">Female</label>
+  <input type="radio" id="other" name="gender" value="other"><label for="other">Other</label><br><br>
 
-<!-- Drop down box -->
+<label for="time">Favourite time of the day:</label>
+<input type="time" id="time" name="time"> <br><br>
+
 <label for="pet">Cats or Dogs?</label>
 <select id="pet" name="pet">
   <option value="Cats">Cats</option>
   <option value="Dogs">Dogs</option>
 </select> <br><br>
 
-<!-- Checkboxes -->
 <h2>From time to time, we may wish to contact you about
 further menial registration forms
 to fill out. If you do not wish to hear from us about these forms,
--- Result.php
+++ Result.php
@@ -5,12 +5,15 @@
 <body style="background-color:#E8D3B9;">
 <h1>Abstract Registration Form Ltd.</h1>
 
+<h2>Thank you for registering!</h2>
 Welcome <?php echo $_POST["name"]; ?><br>
 Your email address is: <?php echo $_POST["email"]; ?><br>
 Your date of birth is: <?php echo $_POST["date"]; ?><br>
 Your website is: <?php echo $_POST["website"]; ?><br>
 Your gender is: <?php echo $_POST["gender"]; ?><br>
+Your favourite time of the day is: <?php echo $_POST["time"]; ?><br>
 You like <?php echo $_POST["pet"]; ?>!<br>
+<!-- TODO: Output this data type more specifically. -->
 You have opted into:
 Additional comments: <?php echo $_POST["comment"]; ?><br>
 
